d4e64dfca72744f27cd2296787e42e3e88929f77
Amirhan Almeev <almeevamirhan294@gmail.com>
2026-06-08 17:15:19 +0300

    Скелет микросервисов + docker-compose

---
+++ anti-fraud-service/anti-fraud-service/src/test/java/com/transactions/anti_fraud_service/AntiFraudServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transactions.anti_fraud_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntiFraudServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
